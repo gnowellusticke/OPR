b48f5fd0137964e77c16c4c60cecac4ef2f0ccbe
Gordon Nowell-Usticke <g.nowellusticke@gmail.com>
2026-02-22 01:43:56 +0000

    File changes

---
+++ src/components/engine/spells/OrcSpells.jsx
@@ -0,0 +1,36 @@
+{
+  "faction": "Orc",
+  "aliases": ["Orc Marauders", "Marauders", "Orcs"],
+  "spells": [
+    {
+      "name": "Waaagh!",
+      "spell_cost": 1,
+      "range": 6,
+      "attacks": 0,
+      "ap": 0,
+      "effect": "buff",
+      "description": "Nearby Orc unit gets Furious until end of round.",
+      "special_rules": ""
+    },
+    {
+      "name": "Gut Bash",
+      "spell_cost": 2,
+      "range": 12,
+      "attacks": 3,
+      "ap": 2,
+      "effect": "damage",
+      "description": "Channels brutal force into a crushing blow.",
+      "special_rules": "AP(2)"
+    },
+    {
+      "name": "Blood Frenzy",
+      "spell_cost": 3,
+      "range": 9,
+      "attacks": 4,
+      "ap": 1,
+      "effect": "damage",
+      "description": "Unleashes a torrent of savage strikes.",
+      "special_rules": "AP(1), Rending"
+    }
+  ]
+}
