50e480896a1eb6563c327d6ebec48987d662d21d
Gordon Nowell-Usticke <g.nowellusticke@gmail.com>
2026-02-22 01:44:28 +0000

    File changes

---
+++ src/components/engine/spells/UndeadSpells.jsx
@@ -0,0 +1,46 @@
+{
+  "faction": "Undead",
+  "aliases": ["Undead Legions", "Legions", "Necromancer", "Skeleton"],
+  "spells": [
+    {
+      "name": "Soul Drain",
+      "spell_cost": 1,
+      "range": 12,
+      "attacks": 2,
+      "ap": 0,
+      "effect": "damage",
+      "description": "Drains the life force from a nearby enemy.",
+      "special_rules": "Bane"
+    },
+    {
+      "name": "Raise Dead",
+      "spell_cost": 2,
+      "range": 6,
+      "attacks": 0,
+      "ap": 0,
+      "effect": "buff",
+      "description": "Restores lost models to a friendly undead unit.",
+      "special_rules": ""
+    },
+    {
+      "name": "Deathbolt",
+      "spell_cost": 2,
+      "range": 18,
+      "attacks": 3,
+      "ap": 2,
+      "effect": "damage",
+      "description": "A bolt of necrotic energy tears through armor.",
+      "special_rules": "AP(2)"
+    },
+    {
+      "name": "Plague Wind",
+      "spell_cost": 3,
+      "range": 12,
+      "attacks": 4,
+      "ap": 1,
+      "effect": "damage",
+      "description": "A virulent cloud ravages enemy ranks.",
+      "special_rules": "AP(1), Blast(4), Bane"
+    }
+  ]
+}
