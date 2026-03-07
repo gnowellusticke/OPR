519f6e5a9f5483cebf3bceabc0f108c4b533abbc
gnowellusticke <g.nowellusticke@gmail.com>
2026-03-08 08:50:46 +1300

    removed path from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,9 +16,5 @@ export default defineConfig({
     }),
     react(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
-});
+);
